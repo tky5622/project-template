5eef8379a984319b492a8d94dc21ec0423c539fb
takuya <>
2022-10-05 12:41:54 +0900

    Add storybooks

---
+++ eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
+    'prettier',
+  ],
+}
